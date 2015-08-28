4f404c9df310f00eeb48dc772e9cae2814d160bd
Andres Bayona <abayona@yahoo.com>
2015-08-27 22:31:11 -0500

    Revising ryu-cool positioning

--- js/app.js
+++ js/app.js
@@ -22,23 +22,19 @@ $(document).ready(function(){
   	.mouseup(function() {
     $('.ryu-throwing').hide();
  	$('.ryu-ready').show();
-  });
+});
 
 $(document).keydown(function(e) {
-	if (e.keyCode == 88) {
-		$('.ryu-ready').hide();
-		$('.ryu-cool').show();
-	}
-}).keyup(function(e) {
-    if (e.keyCode == 88) {
-      $('#cool')[0].pause();
-      $('#cool')[0].load();
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-    }
-  });
-
-
+		if (e.keyCode == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	}).keyup(function(e) {
+	    if (e.keyCode == 88) {
+	      $('.ryu-cool').hide();
+	      $('.ryu-still').show();
+	    }
+ });
 
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
