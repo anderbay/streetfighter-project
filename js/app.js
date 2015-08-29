101114b3a16f4bbb5269a311effb25fa0aeed161
Andres Bayona <abayona@yahoo.com>
2015-08-28 21:41:11 -0500

    Organize jQuery indenting

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
-  	.mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  })
-  	.mousedown(function() {
-    playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
-	  {'left': '1020px'},
-	  500,
-	  function() {
-	    $(this).hide();
-	    $(this).css('left', '520px');
-	  });
-  	})
-  	.mouseup(function() {
-    $('.ryu-throwing').hide();
- 	$('.ryu-ready').show();
-});
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+		})
+		.mouseleave(function() {
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();
+		})
+		.mousedown(function() {
+			playHadouken();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show().animate({
+					'left': '1020px'
+				},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left', '520px');
+				});
+		})
+		.mouseup(function() {
+			$('.ryu-throwing').hide();
+			$('.ryu-ready').show();
+		});
 
-$(document).keydown(function(e) {
+	$(document).keydown(function(e) {
 		if (e.keyCode == 88) {
 			$('.ryu-ready').hide();
 			$('.ryu-cool').show();
 		}
 	}).keyup(function(e) {
-	    if (e.keyCode == 88) {
-	      $('.ryu-cool').hide();
-	      $('.ryu-still').show();
-	    }
- });
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
 
-function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
-    }
-});
+	function playHadouken() {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
 	
