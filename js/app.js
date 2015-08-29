168b3a7b47e84f51cd47c4e2c8bdb4b0681bcf02
Andres Bayona <abayona@yahoo.com>
2015-08-28 22:27:03 -0500

    Fixed ryu duplicates

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
 		})
 		.mouseleave(function() {
 			$('.ryu-ready').hide();
+			$('.ryu-cool').hide();
 			$('.ryu-still').show();
 		})
 		.mousedown(function() {
 			playHadouken();
 			$('.ryu-ready').hide();
+			$('.ryu-cool').hide();
 			$('.ryu-throwing').show();
 			$('.hadouken').finish().show().animate({
 					'left': '1020px'
@@ -22,17 +24,21 @@ $(document).ready(function() {
 		})
 		.mouseup(function() {
 			$('.ryu-throwing').hide();
+			$('.ryu-cool').hide();
 			$('.ryu-ready').show();
 		});
 
 	$(document).keydown(function(e) {
 		if (e.keyCode == 88) {
+			$('.ryu-throwing').hide();
 			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
 			$('.ryu-cool').show();
 		}
 	}).keyup(function(e) {
 		if (e.keyCode == 88) {
 			$('.ryu-cool').hide();
+			$('.ryu-ready').hide();
 			$('.ryu-still').show();
 		}
 	});
@@ -42,4 +48,4 @@ $(document).ready(function() {
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
 	}
-	
+});	
